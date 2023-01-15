ee2afb55cfe967dee35f847490a843d5468a334e
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-01-15 17:38:00 +0530

    working doers app

---
+++ todo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Todo
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields=('created',)
+# Register your models here.
+admin.site.register(Todo,TodoAdmin)
